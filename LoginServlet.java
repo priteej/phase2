f93e5083a82b3bd414d359153199458ff74cc733
priteej <100280187+priteej@users.noreply.github.com>
2022-03-01 19:18:12 +0530

    sessionDemo

--- LoginServlet.java
+++ LoginServlet.java
@@ -1,6 +1,4 @@
 import java.io.*;
-import java.io.PrintWriter;
-
 import javax.servlet.*;
 import javax.servlet.annotation.*;
 import javax.servlet.http.*;
@@ -27,17 +25,12 @@ public class LoginServlet extends HttpServlet {
          */
         protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
                 // TODO Auto-generated method stub
-                 PrintWriter out = response.getWriter();
-                 out.println("<html><body>");
-                    
+                
                  String userId = request.getParameter("userid");
-                 //creating a new hidden form field
-                out.println("<form action='dashboard' method='post'>");
-                out.println("<input type='hidden' name='userid' id='userid' value='"+userId+"'>");
-                out.println("<input type='submit' value='submit' >");
-                out.println("</form>");
-                out.println("<script>document.forms[0].submit();</script>");
-              
+                 HttpSession session=request.getSession();  
+             session.setAttribute("userid",  userId);
+                          
+             response.sendRedirect("dashboard");  
                 
         }
 
@@ -50,8 +43,3 @@ public class LoginServlet extends HttpServlet {
         }
 
 }
-
-
-
-
-
