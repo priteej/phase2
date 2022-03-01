f696194e849817e51c488c8e5016bb9c1a670b11
priteej <100280187+priteej@users.noreply.github.com>
2022-03-01 19:14:37 +0530

    hibrlazyy

--- ProductDetails.java
+++ ProductDetails.java
@@ -23,10 +23,12 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-import com.ecommerce.EProduct;
-import com.ecommerce.HibernateUtil;
-import com.ecommerce.ProductParts;
 
+import com.ecommerce.Color;
+import com.ecommerce.EProduct;
+import com.ecommerce.Finance;
+import com.ecommerce.HibernateUtil;
+import com.ecommerce.PDescription;
 
 /**
 * Servlet implementation class ProductDetails
@@ -55,21 +57,46 @@ public class ProductDetails extends HttpServlet {
                         
                         Session session = factory.openSession();
                         
-                        
                          
                         List<EProduct> list = session.createQuery("from EProduct").list();
                         
                          PrintWriter out = response.getWriter();
                          out.println("<html><body>");
                          
-                         out.println("<b>Component Mapping</b><br>");
+                         out.println("<b>One to One Mapping</b><br>");
                          for(EProduct p: list) {
                                  out.println("ID: " + String.valueOf(p.getID()) + ", Name: " + p.getName() +
                                                  ", Price: " + String.valueOf(p.getPrice()) + ", Date Added: " + p.getDateAdded().toString());
-                                 ProductParts parts = p.getParts();
-                                 out.println("Parts =" + parts.getCpu() + ", " + parts.getHdd() + ", " + parts.getRam());
+                                 PDescription descrip = p.getPdescrip();
+                                 out.println("<br>Description:" + descrip.getDescrip());
+                                 out.println("<hr>");
+                         }                       
+                         out.println("<b>One to Many and Many to One Mapping</b><br>");
+                         for(EProduct p: list) {
+                                 out.println("ID: " + String.valueOf(p.getID()) + ", Name: " + p.getName() +
+                                                 ", Price: " + String.valueOf(p.getPrice()) + ", Date Added: " + p.getDateAdded().toString());                           
+                                 List<Color> colors = p.getColors();
+                                 out.println("<br>Colors: <ul>");
+                                 for(Color c: colors) {
+                                         out.print("<li>" + c.getName() + "</li>");
+                                 }
+                                 out.println("</ul>");
+                                 out.println("<hr>");
+                                 
+                         }
+                         out.println("<b>Many to Many Mapping</b><br>");
+                         for(EProduct p: list) {
+                                 out.println("ID: " + String.valueOf(p.getID()) + ", Name: " + p.getName() +
+                                                 ", Price: " + String.valueOf(p.getPrice()) + ", Date Added: " + p.getDateAdded().toString());
+                                 Set<Finance> finances= p.getFinance();
+                                 out.println("<br>Finance Options : <ul>");
+                                 for(Finance f: finances) {
+                                         out.print("<li>" + f.getFtype() + "</li>");
+                                 }
+                                 out.println("</ul>");
                                  out.println("<hr>");
                          }
+                         
                          
                                 session.close();
 
